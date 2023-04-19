94782b9b805e477ffc1ae09db73115e6188253a3
smallfawn <860562056@qq.com>
2023-04-19 19:25:09 +0800

    .

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>t</div>
+</template>
+
+<script>
+import { createApp } from 'vue';
+import MyMain from './components/my-main/MyMain.vue';
+import axios from 'axios'
+export default {
+  name:'MyApp',
+  created(){
+    console.log('created 代码创建生命周期函数');
+  }
+}
+</script>
+  
+<style>
+
+</style>
