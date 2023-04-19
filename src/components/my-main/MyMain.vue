0f6ab31e3b3085fa2156d1ff7d62a8adb7a404cd
smallfawn <860562056@qq.com>
2023-04-19 21:54:39 +0800

    改变样式

--- src/components/my-main/MyMain.vue
+++ src/components/my-main/MyMain.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
     MyMain
-    <el-input /><span></span>
+    <el-input size="large"/><span></span>
     <div class="flex">
       <el-input /><span></span>
-      <el-button size="large" type=""><span>获取验证码</span></el-button>
+      <el-button size="large" type="" class="el-button"><span>获取验证码</span></el-button>
     </div>
+    <div>
+      <el-button size="large" type="primary" style="width: 100%"
+        ><span>获取验证码</span></el-button
+      >
+    </div>
+    <el-button size="large" style="width: 100%"><span>返回</span></el-button>
   </div>
 </template>
 
@@ -16,9 +22,12 @@ export default {
 </script>
 
 <style lang="less" scoped>
-/*.el-button{
-    //height: var(--el-button-size)
-}*/
+.el-button {
+  //display: flex;
+  //height: var(--el-button-size)
+  padding:15px 20px;
+}
+
 .el-button > span {
   align-items: center;
   display: inline-flex;
